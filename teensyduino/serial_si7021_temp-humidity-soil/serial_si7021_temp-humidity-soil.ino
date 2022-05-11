648fb38edea38cadc55232ec7989867864c423a9
Shawn Greenlee <sgreenle@risd.edu>
2022-05-11 13:05:38 -0400

    soil

--- teensyduino/serial_si7021_temp-humidity-soil/serial_si7021_temp-humidity-soil.ino
+++ teensyduino/serial_si7021_temp-humidity-soil/serial_si7021_temp-humidity-soil.ino
@@ -1,4 +1,7 @@
 #include "Adafruit_Si7021.h"
+#include "Adafruit_seesaw.h"
+
+Adafruit_seesaw ss;
 
 bool enableHeater = false;
 uint8_t loopCnt = 0;
@@ -14,15 +17,23 @@ void setup() {
   }
 
   Serial.println("Si7021 test!");
-  
+
   if (!sensor.begin()) {
     Serial.println("Did not find Si7021 sensor!");
     while (true)
       ;
+
+    if (!ss.begin(0x36)) {
+      Serial.println("ERROR! seesaw not found");
+      while (1) delay(1);
+    } else {
+      Serial.print("seesaw started! version: ");
+      Serial.println(ss.getVersion(), HEX);
+    }
   }
 
   Serial.print("Found model ");
-  switch(sensor.getModel()) {
+  switch (sensor.getModel()) {
     case SI_Engineering_Samples:
       Serial.print("SI engineering samples"); break;
     case SI_7013:
@@ -42,29 +53,49 @@ void setup() {
 }
 
 void loop() {
+
+  // temperature sensor 
+  
+  float humid = sensor.readHumidity();
   Serial.print("humidity: ");
-  Serial.print(sensor.readHumidity());
+  Serial.print(humid);
   Serial.print(" ");
   Serial.println(" ");
 
+  float temp = sensor.readTemperature();
   Serial.print("temperature: ");
-  Serial.print(sensor.readTemperature());
+  Serial.print(temp);
   Serial.print(" ");
   Serial.println(" ");
-  
+
+
+  // soil sensor
+
+  float tempC = ss.getTemp();
+  Serial.print("soiltemp: "); 
+  Serial.print(tempC);
+  Serial.print(" ");
+  Serial.println(" ");
+
+  uint16_t capread = ss.touchRead(0);
+  Serial.print("capacitive: ");
+  Serial.print(capread);
+  Serial.print(" ");
+  Serial.println(" ");
+
   delay(1000);
 
   // Toggle heater enabled state every 30 seconds
   // An ~1.8 degC temperature increase can be noted when heater is enabled
-//  if (++loopCnt == 30) {
-//    enableHeater = !enableHeater;
-//    sensor.heater(enableHeater);
-//    Serial.print("Heater Enabled State: ");
-//    if (sensor.isHeaterEnabled())
-//      Serial.println("ENABLED");
-//    else
-//      Serial.println("DISABLED");
-//       
-//    loopCnt = 0;
-//  }
+  //  if (++loopCnt == 30) {
+  //    enableHeater = !enableHeater;
+  //    sensor.heater(enableHeater);
+  //    Serial.print("Heater Enabled State: ");
+  //    if (sensor.isHeaterEnabled())
+  //      Serial.println("ENABLED");
+  //    else
+  //      Serial.println("DISABLED");
+  //
+  //    loopCnt = 0;
+  //  }
 }
